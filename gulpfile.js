bf24a417906c190fac46f6e03f85ed09bd5bbbd6
vdegenne <vdegenne@gmail.com>
2017-08-26 14:32:54 +0200

    v2.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 /* const browserSync = require('browser-sync');
 const reload = browserSync.reload; */
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 const { init, reload } = require('browser-sync');
 
 gulp.task('clean', () => {
@@ -8,9 +11,18 @@ gulp.task('clean', () => {
      .pipe(gclean());
 });
 
+gulp.task('uglify', _ => {
+  return gulp.src('text-correction-parser.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(rename('text-correction-parser.min.js'))
+    .pipe(gulp.dest('.'));
+});
 
 gulp.task('serve', _ => {
-  
+
   init({
     server: {
       baseDir: './',
@@ -19,5 +31,5 @@ gulp.task('serve', _ => {
   });
 
   gulp.watch(['text-correction-parser.js', 'index.html'], reload);
-  
+
 });
--- text-correction-parser.js
+++ text-correction-parser.js
@@ -1,97 +1,172 @@
 (function () {
   'use strict';
 
-  let operators = [
-    {
-      name: 'wrong_replace',
-      opening : '[',
-      closing: '>',
-      encloser: '<span class="wrong_replace">%</span> '
-    },
-    
-    {
-      name: 'right_replace',
-      opening: '>',
-      closing: ']',
-      encloser: '<span class="right_replace">%</span>'
-    },
-    
-    {
-      name: 'removed',
-      opening: '>',
-      closing: '<',
-      encloser: '<span class="removed">%</span>'
-    },
-    
-    {
-      name: 'adding',
-      opening: '@',
-      closing: '@',
-      encloser: '<span class="added">%</span>'
+  const enclosers = [{
+    name: 'left-wrong-replaced',
+    opening : '[r]',
+    closing: '>>',
+    template: '<span class="left-wrong-replaced">%</span>'
+  },{
+    name: 'right-right-replaced',
+    opening: '>>',
+    closing: '[/r]',
+    template: '<span class="right-right-replaced">%</span>'
+  },{
+    name: 'left-right-replaced',
+    opening : '[r]',
+    closing: '<<',
+    template: '<span class="left-right-replaced">%</span>'
+  },{
+    name: 'right-wrong-replaced',
+    opening: '<<',
+    closing: '[/r]',
+    template: '<span class="right-wrong-replaced">%</span>'
+  },{
+    name: 'deleted',
+    opening: '[d]',
+    closing: '[/d]',
+    template: '<span class="deleted">%</span>'
+  },{
+    name: 'added',
+    opening: '[a]',
+    closing: '[/a]',
+    template: '<span class="added">%</span>'
+  }];
+
+
+
+  class TextCorrectionParser {
+
+    constructor(data) {
+      this.data = data;
+      this.parsed;
+      this.dindex;
+      this.currentCandidates;
+      this.cchar;
+      this.enclosedText;
+
+      this.resetVars();
     }
-  ];
-  
-  let awaiting, candidates, enclosedText;
-  
-  function reset_vars () {
-    awaiting = false;
-    candidates = [];
-    enclosedText = '';
-  }
-  
-  function parseData (data) {
-    let output = '';
-    reset_vars();
-    
-    for (let i = 0; i < data.length; ++i) {
-      let theLetter = data[i];
-      
-      if (!awaiting) {
-        /* searching for an opening */
-        let openings = operators.filter(function (o) {
-          return o.opening === theLetter;
-        });
-        
-        if (openings.length) {
-          candidates = openings;
-          awaiting = true;
+
+    resetVars() {
+      this.parsed = '';
+      this.dindex = 0;
+      this.currentCandidates = [];
+      this.cchar = '';
+      this.enclosedText = '';
+    }
+
+
+    parse(data = this.data) {
+      this.data = data;
+      this.resetVars();
+
+      let possible;
+      let tagStartRegexp;
+      let foundClosingCandidates;
+      let hasJustFoundClosingCandidate = false;
+
+      for (this.dindex = 0; this.dindex < this.data.length; ++this.dindex) {
+
+        this.cchar = this.data[this.dindex];
+        tagStartRegexp = new RegExp('^' + this.escapeRegExp(this.cchar));
+
+        /* check for close candidates */
+        if (this.currentCandidates.length) {
+          possible = enclosers.filter(e => tagStartRegexp.test(e.closing));
+          if (possible.length) {
+            let foundClosingEnclosers = this.findEnclosers('closing');
+            if (foundClosingEnclosers.length) { // a closing tag was found amonst the candidates
+              foundClosingCandidates = this.currentCandidates.filter(c => {
+                for (let f in foundClosingEnclosers) {
+                  if (c === foundClosingEnclosers[f]) {
+                    return true;
+                  }
+                }
+                return false;
+              });
+              if (foundClosingCandidates.length) {
+                this.parsed += foundClosingCandidates[0].template.replace('%', this.enclosedText);
+                this.currentCandidates = [];
+                hasJustFoundClosingCandidate = true;
+
+                this.enclosedText = '';
+              }
+              else {
+                hasJustFoundClosingCandidate = false;
+                this.addCharToEnclosedText();
+                continue;
+              }
+            }
+            else {
+              this.addCharToEnclosedText();
+              continue;
+            }
+          }
+          else {
+            this.addCharToEnclosedText();
+            continue;
+          }
+        }
+
+        /* we search for opening tags */
+        possible = enclosers.filter(e => tagStartRegexp.test(e.opening));
+        if (possible.length) {
+          this.currentCandidates = this.findEnclosers('opening');
+          if (this.currentCandidates.length) {
+            // jumping the tag
+            this.dindex += this.currentCandidates[0].opening.length - 1;
+            continue;
+          }
+        }
+
+        if (hasJustFoundClosingCandidate) {
+          this.dindex += foundClosingCandidates[0].closing.length - 1;
+          hasJustFoundClosingCandidate = false;
           continue;
         }
-        
-        output += theLetter;
-        continue;
+
+        this.parsed += this.cchar;
       }
-      
-      /* else we search for closers */
-      let closers = candidates.filter(function (c) {
-        return c.closing === theLetter;
-      });
-      
-      if (closers.length)
-        {
-        let closer = closers[0];
-        
-        output += closer.encloser.replace('%', enclosedText);
-        reset_vars();
-        
-        let openings = operators.filter(function (o) {
-          return (!(closer === o) && o.opening === theLetter);
-        });
-        
-        if (openings.length) {
-          candidates = openings;
-          awaiting = true;
-        }
-        continue;
-      }
-      
-      enclosedText += theLetter;
+
+
+      return this.parsed;
     }
-    
-    reset_vars();
-    return output;
+
+    addCharToEnclosedText() {
+      this.enclosedText += this.cchar;
+    }
+
+    findEnclosers(type = 'opening') {
+      let offset = 0;
+      let tag = '';
+      let founds, possible;
+      let tagStartRegexp;
+
+      do {
+        tag += this.data[this.dindex + offset];
+        offset++;
+
+        /* are there some enclosers ? */
+        founds = enclosers.filter(e => e[type] === tag);
+        if (founds.length) {
+          return founds;
+        }
+        /* if no founds, let's see if there are some possible enclosers */
+        tagStartRegexp = new RegExp('^' + this.escapeRegExp(tag));
+        possible = enclosers.filter(e => tagStartRegexp.test(e[type]));
+
+        // we continue while possible enclosers
+      } while (possible.length);
+
+      return [];
+    }
+
+    escapeRegExp(s) {
+      return s.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    }
   }
 
-  window.TextCorrectionParser = window.TextCorrectionParser || {};
-  TextCorrectionParser.parse = parseData;
+
+  window.TextCorrectionParser = TextCorrectionParser;
 })();
